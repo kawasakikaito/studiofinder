3c57343460fdf7b149b88e5336e5dfbab5802ebd
Kaito <kaito.kawasaki09@gmail.com>
2024-11-16 10:39:07 +0900

    reactの名称変更

--- studionavi/wsgi.py
+++ studionavi/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "studiofinder.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "studionavi.settings")
 
 application = get_wsgi_application()
